1e59f17757f34120c2e495b98afc933333b1672f
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-08-14 10:00:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const SudokuSolver = require("./SudokuSolver/SudokuSolver");
 const app = express();
 
 const port = 3000;
@@ -7,7 +8,7 @@ app.listen(port, () => console.log("App is listening now!"));
 
 app.get("/", function (req, res) {
   res.send(
-    "Normal usage of the API is send a string with get request to the solve endpoint, with the stringified puzzle e.g. apipath/solve/....5.4.5.6.5.4..4.6.6.4.5.2.3....5.56...4.4..6..6."
+    "Normal usage of the API is send a string with get request to the solve endpoint, with the stringified puzzle e.g. apipath/solve/2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"
   );
 });
 
@@ -15,10 +16,11 @@ app.get("/solve", function (req, res) {
   res.send("You forgot the string what describes, the puzzle.");
 });
 
-app.get("/solve/:id", function (req, res, next) {
-  res.send("Requested id: " + req.params.id);
+app.get("/solve/:id", function (req, res) {
   console.log("Requested id: " + req.params.id);
-  next();
+  const sudoku = new SudokuSolver({ sectionSizeX: 3, sectionSizeY: 3 });
+  const solution = sudoku.solvePuzzle(req.params.id, "string");
+  res.send("Requested id: " + req.params.id);
 });
 
 app.use(express.urlencoded({ extended: true }));
