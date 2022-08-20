bd819d44ef02ee44a836126730960428f924300e
exphoenee <bozzay.viktor@gmail.com>
2022-08-20 19:18:54 +0200

    Create BoardCreation.test.mjs

---
+++ src/SudokuBoard/tests/BoardCreation.test.mjs
@@ -0,0 +1,34 @@
+"use strict";
+
+import SudokuBoard from "../SudokuBoard.mjs";
+import {
+  puzzle2d,
+  puzzle1d,
+  puzzleStr,
+  unsolvable2d,
+  unsolvable1d,
+  unsolvableStr,
+  wrong2d,
+  wrong1d,
+  wrongStr,
+} from "../../Model/Puzzles.mjs";
+
+//console.log(puzzle2d, puzzle1d, puzzleStr);
+//console.log("-----------");
+console.log("Created board, then filled with values.");
+const board = new SudokuBoard(3, 3);
+board.setBoard(puzzle2d);
+console.log(board.getCellValues({ format: "string" }));
+
+console.log("Created board2 with values.");
+const board2 = new SudokuBoard(3, 3, puzzleStr);
+console.log(board2.getCellValues({ format: "string" }));
+
+console.log("Created board3, then filled with values.");
+const board3 = new SudokuBoard(3, 3);
+board3.setBoard(puzzle1d);
+console.log(board3.getCellValues({ format: "string" }));
+
+console.log("Created board4, then filled with values.");
+const board4 = new SudokuBoard(3, 3, puzzle1d);
+console.log(board3.getCellValues({ format: "string" }));
