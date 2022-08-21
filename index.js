cee2513cf9686a5d2ab3ccbd39063df82a06464b
exphoenee <bozzay.viktor@gmail.com>
2022-08-21 11:19:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,72 @@ const app = express();
 const port = 3000;
 
 app.listen(port, () => console.log("App is listening now!"));
+/*
+app.get("/", function (req, res) {
+  res.send(
+    "Normal usage of the API is send a string with get request to the solve endpoint, with the stringified puzzle and the dimension x and y of the sudoku board e.g.: apipath/solve/2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3/9/9,\nyou can set the unfilled values to '0' cahracter, the default is '.'\n"
+  );
+});
+*/
 
 app.get("/", function (req, res) {
   res.send(
-    "Normal usage of the API is send a string with get request to the solve endpoint, with the stringified puzzle e.g. apipath/solve/2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"
+    "Normal usage of the API is send a string with get request to the solve endpoint, with the stringified puzzle e.g.: apipath/solve/2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3,\nyou can set the unfilled values to '0' cahracter, the default is '.'\n"
   );
 });
 
 app.get("/solve", function (req, res) {
-  res.send("You forgot the string what describes, the puzzle.");
+  res.send(
+    "You forgot the string what describes, the puzzle, the xdimension and y dimension of the puzzle."
+  );
 });
 
-app.get("/solve/:id", function (req, res) {
-  console.log("Requested id: " + req.params.id);
-  const sudoku = new SudokuSolver({ sectionSizeX: 3, sectionSizeY: 3 });
-  const solution = sudoku.solvePuzzle(req.params.id, "string");
-  res.send("Requested id: " + req.params.id);
+/*
+app.get("/solve/:puzzle/:xdim/:ydim", function (req, res) {
+  const { puzzle, xdim, ydim } = {
+    puzzle: req.params.puzzle,
+    xdim: req.params.xdim,
+    ydim: req.params.ydim,
+  };
+
+  puzzle.replace(/./g, 0);
+
+  const sudoku = new SudokuSolver(Math.sqrt(xdim), Math.sqrt(ydim));
+  const solution = sudoku.solvePuzzle({
+    puzzle,
+    format: "string",
+    unfilledChar: ".",
+  });
+  console.log(
+    "Requested puzzle: " + puzzle,
+    "x: " + xdim,
+    "y: " + ydim,
+    "Solution: " + solution
+  );
+  res.send(solution);
+});
+*/
+
+app.get("/solve/:puzzle", function (req, res) {
+  const { puzzle } = {
+    puzzle: req.params.puzzle,
+  };
+  const xdim = 9;
+  const ydim = 9;
+
+  const sudoku = new SudokuSolver(Math.sqrt(xdim), Math.sqrt(ydim));
+  const solution = sudoku.solvePuzzle({
+    puzzle,
+    format: "string",
+    unfilledChar: ".",
+  });
+  console.log(
+    "Requested puzzle: " + puzzle,
+    "x: " + xdim,
+    "y: " + ydim,
+    "Solution: " + solution
+  );
+  res.send(solution);
 });
 
 app.use(express.urlencoded({ extended: true }));
