6db97a46e59e669b4ce7e48ef93e75852698a65b
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-08-22 16:53:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", function (req, res) {
-  res.send(
-    "Normal usage of the API is send a string with get request to the solve endpoint, with the stringified puzzle, the values separated with coma e.g.: apipath/solve/2,.,.,.,.,.,.,.,.,.,.,.,.,.,6,2,.,.,.,.,1,.,.,.,.,7,.,.,.,6,.,.,8,.,.,.,3,.,.,.,9,.,.,.,7,.,.,.,6,.,.,4,.,.,.,4,.,.,.,.,8,.,.,.,.,5,2,.,.,.,.,.,.,.,.,.,.,.,.,.,3"
-  );
+  res.json({
+    error:
+      "Normal usage of the API is send a string with get request to the solve endpoint, with the stringified puzzle, the values separated with coma e.g.: apipath/solve/2,.,.,.,.,.,.,.,.,.,.,.,.,.,6,2,.,.,.,.,1,.,.,.,.,7,.,.,.,6,.,.,8,.,.,.,3,.,.,.,9,.,.,.,7,.,.,.,6,.,.,4,.,.,.,4,.,.,.,.,8,.,.,.,.,5,2,.,.,.,.,.,.,.,.,.,.,.,.,.,3",
+  });
 });
 
 app.get("/solve", function (req, res) {
-  res.send(
-    "You forgot the string what describes, the puzzle, the xdimension and y dimension of the puzzle."
-  );
+  res.json({
+    error:
+      "You forgot the string what describes, the puzzle, the xdimension and y dimension of the puzzle.",
+  });
 });
 
 app.get("/solve/:puzzle", function (req, res) {
@@ -40,7 +42,7 @@ app.get("/solve/:puzzle", function (req, res) {
     "y: " + ydim,
     "Solution: " + solution
   );
-  res.send(solution);
+  res.json({ solution });
 });
 
 app.listen(PORT, () => console.log(`App is listening now on port ${PORT}!`));
