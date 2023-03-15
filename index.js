3f8a0b85571b6222c8868119654a78a566e8a9c4
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-03-15 14:37:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import SudokuGenerator from "./src/SudokuGenerator/SudokuGenerator.mjs";
 
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
